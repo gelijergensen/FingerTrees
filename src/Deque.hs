541a239831d71bf1424215f483b40db0c48914a6
G. Eli Jergensen <elijergensen@gmail.com>
2021-06-02 16:36:34 +0200

    Added traversable instances and ViewL|R

--- src/Deque.hs
+++ src/Deque.hs
@@ -28,6 +28,7 @@ module Deque
   )
 where
 
+import Control.Applicative (liftA2)
 import qualified Data.Bifunctor as Bifunc
 import Data.Maybe (fromJust)
 import qualified FingerTree as Base
@@ -45,6 +46,17 @@ newtype Elem a = Elem
 
 newtype Deque a
   = Deque (Base.FingerTree Size (Elem a))
+  deriving (Eq)
+
+data ViewL a
+  = NilL
+  | ConsL a (Deque a)
+  deriving (Eq, Show)
+
+data ViewR a
+  = NilR
+  | ConsR (Deque a) a
+  deriving (Eq, Show)
 
 instance Semigroup Size where
   Size x <> Size y = Size $ x + y
@@ -75,9 +87,43 @@ instance Foldable Deque where
 instance Functor Deque where
   fmap f (Deque xs) = Deque $ Bifunc.second (fmap f) xs
 
+instance Traversable Deque where
+  traverse _ Empty = pure Empty
+  traverse f (x :<| xs) = liftA2 (:<|) (f x) (traverse f xs)
+
 instance (Show a) => Show (Deque a) where
   showsPrec p xs =
     showParen (p > 10) $ showString "fromList " . shows (toList xs)
+
+instance Functor ViewL where
+  fmap _ NilL = NilL
+  fmap f (ConsL x xs) = ConsL (f x) (fmap f xs)
+
+instance Foldable ViewL where
+  foldr _ z NilL = z
+  foldr f z (ConsL x xs) = f x (foldr f z xs)
+
+  foldl _ z NilL = z
+  foldl f z (ConsL x xs) = foldl f (f z x) xs
+
+instance Traversable ViewL where
+  traverse _ NilL = pure NilL
+  traverse f (ConsL x xs) = liftA2 ConsL (f x) (traverse f xs)
+
+instance Functor ViewR where
+  fmap _ NilR = NilR
+  fmap f (ConsR xs x) = ConsR (fmap f xs) (f x)
+
+instance Foldable ViewR where
+  foldr _ z NilR = z
+  foldr f z (ConsR xs x) = foldr f (f x z) xs
+
+  foldl _ z NilR = z
+  foldl f z (ConsR xs x) = f (foldl f z xs) x
+
+instance Traversable ViewR where
+  traverse _ NilR = pure NilR
+  traverse f (ConsR xs x) = liftA2 ConsR (traverse f xs) (f x)
 
 empty :: Deque a
 empty = Deque Base.Empty
@@ -105,7 +151,7 @@ pattern (:<|) ::
   Deque a ->
   Deque a
 pattern x :<| xs <-
-  (viewL -> (x, xs))
+  (viewL -> ConsL x xs)
   where
     x :<| xs = x <| xs
 
@@ -117,7 +163,7 @@ pattern (:|>) ::
   a ->
   Deque a
 pattern xs :|> x <-
-  (viewR -> (xs, x))
+  (viewR -> ConsR xs x)
   where
     xs :|> x = xs |> x
 
@@ -152,15 +198,15 @@ infixr 5 ><
 (><) :: Deque a -> Deque a -> Deque a
 (Deque xs) >< (Deque ys) = Deque $ xs Base.>< ys
 
-viewL :: Deque a -> (a, Deque a)
-viewL (Deque xs) = (getElem x, Deque xs')
-  where
-    x Base.:<| xs' = xs
+viewL :: Deque a -> ViewL a
+viewL (Deque xs) = case xs of
+  Base.Empty -> NilL
+  x Base.:<| xs' -> ConsL (getElem x) (Deque xs')
 
-viewR :: Deque a -> (Deque a, a)
-viewR (Deque xs) = (Deque xs', getElem x)
-  where
-    xs' Base.:|> x = xs
+viewR :: Deque a -> ViewR a
+viewR (Deque xs) = case xs of
+  Base.Empty -> NilR
+  xs' Base.:|> x -> ConsR (Deque xs') (getElem x)
 
 {- O(1) -}
 head :: Deque a -> a
